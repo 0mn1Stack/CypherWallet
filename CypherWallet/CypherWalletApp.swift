4056cf125f02a0bfb0191f253afc8191cf5f7a3d
srollins25 <81390904+0mn1Stack@users.noreply.github.com>
2021-04-27 00:09:01 -0700

    Initial Commit

---
+++ CypherWallet/CypherWalletApp.swift
@@ -0,0 +1,17 @@
+//
+//  CypherWalletApp.swift
+//  CypherWallet
+//
+//  Created by stephan rollins on 4/27/21.
+//
+
+import SwiftUI
+
+@main
+struct CypherWalletApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
